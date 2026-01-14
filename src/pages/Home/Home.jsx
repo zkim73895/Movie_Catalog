fc6bf49ff4a5ded29b93e77388a3021176705082
zkim7 <kime23199@gmail.com>
2026-01-14 20:07:43 +0500

    adding json-server and movies loading with error handling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import { getMovies } from "../../services/movieApi.js";
+
+
 const Home = () => {
+    const [movie, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        getMovies()
+            .then(data => setMovies(data))
+            .catch(err => setError(err.message))
+            .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <p>Загрузка...</p>
+    if (error) return <p>{error}</p>
+
   return (
     <section className="home">
-      <h2 className="home__title">Список фильмов</h2>
-      <p>Здесь будет список фильмов</p>
+        <h2 className="home__title">Список фильмов</h2>
+
+        <ul className={"home_list"}>
+            {movie.map(movie => (
+                <li key={movie.id}>{movie.title}</li>
+            ))}
+        </ul>
     </section>
   )
 }
---
+++ src/services/movieApi.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://localhost:3001/movies'
+
+export const getMovies = async () => {
+    const response = await fetch(API_URL)
+
+    if (!response.ok) {
+        throw new Error('Ошибка загрузки')
+    }
+
+    return response.json()
+}
